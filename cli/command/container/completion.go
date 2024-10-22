ebca4829e1ae91ef0add39eae566716cc617e765
Harald Albers <github@albersweb.de>
2024-10-22 19:40:31 +0000

    Share the container completions
    Signed-off-by: Harald Albers <github@albersweb.de>

--- cli/command/container/completion.go
+++ cli/command/container/completion.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/docker/cli/cli/command"
 	"github.com/docker/cli/cli/command/completion"
 	"github.com/docker/docker/api/types/container"
 	"github.com/moby/sys/capability"
@@ -52,6 +53,20 @@ var restartPolicies = []string{
 	string(container.RestartPolicyAlways),
 	string(container.RestartPolicyOnFailure),
 	string(container.RestartPolicyUnlessStopped),
+}
+
+// addCompletions adds the completions that `run` and `create`have in common.
+func addCompletions(cmd *cobra.Command, dockerCli command.Cli) {
+	_ = cmd.RegisterFlagCompletionFunc("cap-add", completeLinuxCapabilityNames)
+	_ = cmd.RegisterFlagCompletionFunc("cap-drop", completeLinuxCapabilityNames)
+	_ = cmd.RegisterFlagCompletionFunc("env", completion.EnvVarNames)
+	_ = cmd.RegisterFlagCompletionFunc("env-file", completion.FileNames)
+	_ = cmd.RegisterFlagCompletionFunc("network", completion.NetworkNames(dockerCli))
+	_ = cmd.RegisterFlagCompletionFunc("platform", completion.Platforms)
+	_ = cmd.RegisterFlagCompletionFunc("pull", completion.FromList(PullImageAlways, PullImageMissing, PullImageNever))
+	_ = cmd.RegisterFlagCompletionFunc("restart", completeRestartPolicies)
+	_ = cmd.RegisterFlagCompletionFunc("stop-signal", completeSignals)
+	_ = cmd.RegisterFlagCompletionFunc("volumes-from", completion.ContainerNames(dockerCli, true))
 }
 
 func completeLinuxCapabilityNames(cmd *cobra.Command, args []string, toComplete string) (names []string, _ cobra.ShellCompDirective) {

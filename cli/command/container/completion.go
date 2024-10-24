4d75984c525dcccd708268ed34156332b9471dd4
Harald Albers <github@albersweb.de>
2024-10-24 18:23:22 +0000

    Disable file completion for `--gpus`
    Signed-off-by: Harald Albers <github@albersweb.de>

--- cli/command/container/completion.go
+++ cli/command/container/completion.go
@@ -87,6 +87,7 @@ func addCompletions(cmd *cobra.Command, dockerCli command.Cli) {
 	_ = cmd.RegisterFlagCompletionFunc("expose", completion.NoComplete)
 	_ = cmd.RegisterFlagCompletionFunc("env", completion.EnvVarNames)
 	_ = cmd.RegisterFlagCompletionFunc("env-file", completion.FileNames)
+	_ = cmd.RegisterFlagCompletionFunc("gpus", completion.NoComplete)
 	_ = cmd.RegisterFlagCompletionFunc("network", completion.NetworkNames(dockerCli))
 	_ = cmd.RegisterFlagCompletionFunc("platform", completion.Platforms)
 	_ = cmd.RegisterFlagCompletionFunc("pull", completion.FromList(PullImageAlways, PullImageMissing, PullImageNever))

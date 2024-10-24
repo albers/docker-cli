f4817bf10f2f697854369d2f383db8555a5c411b
Harald Albers <github@albersweb.de>
2024-10-24 13:34:38 +0000

    Disable file completion for `--security-opt`
    Signed-off-by: Harald Albers <github@albersweb.de>

--- cli/command/container/completion.go
+++ cli/command/container/completion.go
@@ -123,6 +123,7 @@ func addCompletions(cmd *cobra.Command, dockerCli command.Cli) {
 	_ = cmd.RegisterFlagCompletionFunc("pull", completion.FromList(PullImageAlways, PullImageMissing, PullImageNever))
 	_ = cmd.RegisterFlagCompletionFunc("restart", completeRestartPolicies)
 	_ = cmd.RegisterFlagCompletionFunc("runtime", completion.NoComplete)
+	_ = cmd.RegisterFlagCompletionFunc("security-opt", completion.NoComplete) // TODO refine
 	_ = cmd.RegisterFlagCompletionFunc("stop-signal", completeSignals)
 	_ = cmd.RegisterFlagCompletionFunc("volumes-from", completion.ContainerNames(dockerCli, true))
 }

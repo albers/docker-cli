8ba9dae3f44908189fb67c829cd4891224547e47
Harald Albers <github@albersweb.de>
2024-10-24 13:02:11 +0000

    Disable file completion for `--network-alias`
    Signed-off-by: Harald Albers <github@albersweb.de>

--- cli/command/container/completion.go
+++ cli/command/container/completion.go
@@ -114,6 +114,7 @@ func addCompletions(cmd *cobra.Command, dockerCli command.Cli) {
 	_ = cmd.RegisterFlagCompletionFunc("mount", completion.NoComplete)
 	_ = cmd.RegisterFlagCompletionFunc("name", completion.NoComplete)
 	_ = cmd.RegisterFlagCompletionFunc("network", completion.NetworkNames(dockerCli))
+	_ = cmd.RegisterFlagCompletionFunc("network-alias", completion.NoComplete)
 	_ = cmd.RegisterFlagCompletionFunc("platform", completion.Platforms)
 	_ = cmd.RegisterFlagCompletionFunc("pull", completion.FromList(PullImageAlways, PullImageMissing, PullImageNever))
 	_ = cmd.RegisterFlagCompletionFunc("restart", completeRestartPolicies)
